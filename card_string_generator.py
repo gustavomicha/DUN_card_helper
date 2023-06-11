5fb232bce2ce75c89ec67a434dc97e97eb33b2c2
gustavomicha <gustavo@bumper.fi>
2023-06-11 19:47:13 -0300

    added second deck: peligro en campaña

---
+++ card_string_generator.py
@@ -0,0 +1,14 @@
+import os
+
+folder_path = "encuentros/peligro_camp"
+
+file_paths = []
+
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        file_path = os.path.join(root, file)
+        file_path = file_path.replace("\\", "/")  # Replace backslashes with forward slashes
+        if "dorso" not in file_path:
+            file_paths.append(file_path)
+
+print(file_paths)
