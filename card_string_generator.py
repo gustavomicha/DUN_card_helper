5d3867324cb9f22f092562940b426a1b27d88db1
Gus <gustavomicha@gmail.com>
2023-07-14 08:29:32 -0300

    ESP spells updated

--- card_string_generator.py
+++ card_string_generator.py
@@ -1,15 +1,27 @@
 import os
 
-folder_path = "ENG/spells/underworld"#"items/distancia_y_arrojadizas"
+# folder_path = "ESP/hechizos/"#"items/distancia_y_arrojadizas"
 
-file_paths = []
+# file_paths = []
 
-for root, dirs, files in os.walk(folder_path):
-    for file in files:
-        file_path = os.path.join(root, file)
-        file_path = file_path.replace("\\", "/")  # Replace backslashes with forward slashes
-        if ("dorso" and "back") not in file_path:
-            file_paths.append(file_path)
+# for root, dirs, files in os.walk(folder_path):
+#     for file in files:
+#         file_path = os.path.join(root, file)
+#         file_path = file_path.replace("\\", "/")  # Replace backslashes with forward slashes
+#         if ("dorso" and "back") not in file_path:
+#             file_paths.append(file_path)
 
-print(file_paths)
+# print(file_paths)
 
+
+folder_path = "ESP/hechizos/"
+output_string = ""
+
+for folder_name in os.listdir(folder_path):
+    folder_dir = os.path.join(folder_path, folder_name)
+    if os.path.isdir(folder_dir):
+        image_files = os.listdir(folder_dir)
+        image_list = [os.path.join(folder_dir, image) for image in image_files]
+        output_string += f"var {folder_name}Images = {image_list};\n"
+
+print(output_string)
