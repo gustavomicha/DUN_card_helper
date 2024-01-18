9005455416e2b71d0c7bfd5114bb57969b9384fb
Gus <gustavomicha@gmail.com>
2024-01-18 18:48:50 -0300

    refactor: index.html spaghetti code split into several scripts

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
-# DUN_card_helper
-helper web for DUN
-- Main code is split between index.html and hechizos.html
-- index_old is a playground for testing
-- card_string_generator.py is used only to fetch the files' (cards) names from their folders
+# DUN Card Helper
+
+## About
+DUN Card Helper is a fanmade web application designed to facilitate the gameplay of the board game Dungeon Universalis (DUN) developed and distributed by Ludic Dragon (all rights reserved). This project is developed with the assistance of ChatGPT and aims to provide a convenient tool for players, the designer has no prior HTML or JavaScript experience.
+
+## Features
+- **Card Display:** Allows players to view main cards and select them randomly, reducing the time spent on shuffling decks.
+- **Spell Viewer:** Enables viewing of all spells from a chosen magic school, including a mana counter for tracking.
+- **Dual Language Support:** The application is available in both Spanish (index.html) and English (index_EN.html).
+- **Expansion Content:** (currently only in Spanish) Includes content from the game's expansion `New Challenges`.
+
+## Structure
+- `index.html` and `index_EN.html`: Main HTML files for the Spanish and English versions, respectively.
+- `spells.html` and `spells_EN.html`: Sub-html scripts for displaying spells.
+- `script.js` and `card_names.js`: JavaScript files containing functionality and card names (used for spanish version only).
+- `style.css`: Contains the CSS styles used for the layout and design of the web application (for for spanish version only).
+- `index_experiment.html`: A test playground for new features and experiments.
+- `card_string_generator.py`: A Python script to fetch card filenames from their directories.
+- `card_downloader.py`: A Python script to download card images from the official DUN website.
+
+## Notes
+- The English version code (`index_EN.html`) is currently a mixture of HTML, JavaScript, and inline styles, particularly in the English version, as it was created while the author was learning. Despite this, it is fully functional. The Spanish version code is split in HTML (`index.html`), java (`script.js` and `card_names.js`), and CSS (`style.css`). The plan is to change the English version in the future too, and reduce the redundant code between the two versions.
+- All card images are the property of Ludic Dragon. This app should only be used by individuals who own a copy of DUN.
+- Im not a big fan of the "Macondo" font, however I used this for consistency with the offical DUN website.
+
+## Disclaimer
+This is an amateur, fanmade project and may contain incoherent, redundant, and spaghetti code due to the developer's learning process.
---
+++ script.js
@@ -0,0 +1,246 @@
+function populateImageDropdown(deck) {
+    var dropdown = document.getElementById(`imageDropdown_${deck}`);
+    dropdown.innerHTML = ""; // Clear existing options
+
+    // Add default option
+    var defaultOption = document.createElement("option");
+    defaultOption.disabled = true;
+    defaultOption.selected = true;
+    defaultOption.hidden = true;
+    defaultOption.text = "Seleccionar carta";
+    dropdown.add(defaultOption);
+
+    var deckImages = images[deck];
+    if (includeExpansion && images[`${deck}_exp`]) {
+        deckImages = deckImages.concat(images[`${deck}_exp`]);
+    }
+
+    // Sort deckImages array alphabetically
+    deckImages.sort(function(a, b) {
+        var nameA = a.toLowerCase();
+        var nameB = b.toLowerCase();
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+        return 0;
+    });
+
+    deckImages.forEach(function (image) {
+        var option = document.createElement("option");
+        var imageName = image.split("/").pop().replace(/\.(png|jpg)$/i, '');
+        imageName = imageName.replace(/_/g, ' ').replace(/\((\d+)\)/g, ' $1');
+        imageName = capitalizeFirstLetter(imageName);
+        option.text = imageName;
+        option.value = image;
+        dropdown.add(option);
+    });
+}
+
+function clearDropdownAndImageEncuentros(deck) {
+    var dropdown = document.getElementById(`imageDropdown_${deck}`);
+    dropdown.selectedIndex = 0;
+    var selectedImageSrc = "ESP/encuentros/" + deck + "/dorso.png";
+    document.getElementById(`selectedImage_${deck}`).src = selectedImageSrc;
+}
+
+function clearDropdownAndImageMobiliario(deck) {
+    var dropdown = document.getElementById(`imageDropdown_${deck}`);
+    dropdown.selectedIndex = 0;
+    var selectedImageSrc = "ESP/mobiliario/" + deck + "/dorso.png";
+    document.getElementById(`selectedImage_${deck}`).src = selectedImageSrc;
+}
+
+function clearDropdownAndImageItems(deck) {
+    var dropdown = document.getElementById(`imageDropdown_${deck}`);
+    dropdown.selectedIndex = 0;
+    var selectedImageSrc = "ESP/items/" + deck + "/dorso.png";
+    document.getElementById(`selectedImage_${deck}`).src = selectedImageSrc;
+}
+
+function capitalizeFirstLetter(string) {
+    return string.replace(/\b\w/g, function (l) {
+        return l.toUpperCase();
+    });
+}
+
+function changeRandomImage(deck) {
+    var dropdown = document.getElementById(`imageDropdown_${deck}`);
+    var validOptions = Array.from(dropdown.options).filter(option => !option.disabled);
+    
+    if (validOptions.length > 0) {
+        // Show the loading GIF container
+        var loadingGifContainer = document.getElementById('loadingGifContainer');
+        loadingGifContainer.style.display = 'block';
+
+        // Set a timeout to simulate loading time with a delay of 1 second
+        setTimeout(function () {
+            var randomIndex = Math.floor(Math.random() * validOptions.length);
+            dropdown.selectedIndex = Array.from(dropdown.options).indexOf(validOptions[randomIndex]);
+            var selectedImageSrc = dropdown.value;
+            document.getElementById(`selectedImage_${deck}`).src = selectedImageSrc;
+
+            // Hide the loading GIF container after setting the image
+            loadingGifContainer.style.display = 'none';
+        }, 300); // 1000 milliseconds (1 second)
+    }
+}
+
+function changeSelectedImage(deck) {
+    var dropdown = document.getElementById(`imageDropdown_${deck}`);
+    var selectedImageSrc = dropdown.value;
+    document.getElementById(`selectedImage_${deck}`).src = selectedImageSrc;
+}
+
+function showEncuentrosSubMenu() {
+    document.getElementById("mainMenu").style.display = "none";
+    document.getElementById("mobiliarioSubmenu").style.display = "none";
+    document.getElementById("itemsSubmenu").style.display = "none";
+    document.getElementById("infoButton").style.visibility = "hidden";
+
+    document.querySelectorAll('.deckPage').forEach(function (page) {
+        page.style.display = "none";
+    });
+
+    document.getElementById("encuentrosSubmenu").style.display = "block";
+    document.getElementById("encuentrosMenuSelect").selectedIndex = 0;
+    document.getElementById("languageButton").style.display = "none";
+}
+
+function showMobiliarioSubMenu() {
+    document.getElementById("mainMenu").style.display = "none";
+    document.getElementById("encuentrosSubmenu").style.display = "none";
+    document.getElementById("itemsSubmenu").style.display = "none";
+    document.getElementById("infoButton").style.visibility = "hidden";
+    document.getElementById("languageButton").style.display = "none";
+
+    document.querySelectorAll('.deckPage').forEach(function (page) {
+        page.style.display = "none";
+    });
+
+    document.getElementById("mobiliarioSubmenu").style.display = "block";
+    document.getElementById("mobiliarioMenuSelect").selectedIndex = 0;
+}
+
+function showItemsSubMenu() {
+    document.getElementById("mainMenu").style.display = "none";
+    document.getElementById("encuentrosSubmenu").style.display = "none";
+    document.getElementById("mobiliarioSubmenu").style.display = "none";
+    document.getElementById("infoButton").style.visibility = "hidden";
+    document.getElementById("languageButton").style.display = "none";
+
+
+    document.querySelectorAll('.deckPage').forEach(function (page) {
+        page.style.display = "none";
+    });
+
+    document.getElementById("itemsSubmenu").style.display = "block";
+    document.getElementById("itemsMenuSelect").selectedIndex = 0;
+}
+
+function showMainMenu() {
+    document.getElementById("encuentrosSubmenu").style.display = "none";
+    document.getElementById("mobiliarioSubmenu").style.display = "none";
+    document.getElementById("itemsSubmenu").style.display = "none";
+
+    document.querySelectorAll('.deckPage').forEach(function (page) {
+        page.style.display = "none";
+    });
+    document.getElementById("mainMenu").style.display = "block";
+    document.getElementById("infoButton").style.visibility = "visible";
+    document.getElementById("languageButton").style.display = "block";
+
+}
+
+function showEncuentrosDeckPage(deck) {
+    console.log("showEncuentrosDeckPage called with deck:", deck); // Add this to see if function is called with correct deck
+    document.getElementById("encuentrosSubmenu").style.display = "none";
+    document.getElementById("mainMenu").style.display = "none";
+    document.querySelectorAll('.deckPage').forEach(function (page) {
+        page.style.display = "none";
+    });
+
+    var deckPageId = `deckPage_${deck}`;
+    document.getElementById(deckPageId).style.display = "block";
+
+    var selectedImageSrc = "ESP/encuentros/" + deck + "/dorso.png";
+    document.getElementById(`selectedImage_${deck}`).src = selectedImageSrc;
+
+    clearDropdownAndImageEncuentros(deck);
+    populateImageDropdown(deck);
+}
+
+function showMobiliarioDeckPage(deck) {
+    document.getElementById("mobiliarioSubmenu").style.display = "none";
+    document.getElementById("mainMenu").style.display = "none";
+    document.querySelectorAll('.deckPage').forEach(function (page) {
+        page.style.display = "none";
+    });
+
+    var deckPageId = `deckPage_${deck}`;
+    document.getElementById(deckPageId).style.display = "block";
+
+    var selectedImageSrc = "ESP/mobiliario/" + deck + "/dorso.png";
+    document.getElementById(`selectedImage_${deck}`).src = selectedImageSrc;
+
+    clearDropdownAndImageMobiliario(deck);
+    populateImageDropdown(deck);
+}
+
+function showItemsDeckPage(deck) {
+    document.getElementById("itemsSubmenu").style.display = "none";
+    document.getElementById("mainMenu").style.display = "none";
+    document.querySelectorAll('.deckPage').forEach(function (page) {
+        page.style.display = "none";
+    });
+
+    var deckPageId = `deckPage_${deck}`;
+    document.getElementById(deckPageId).style.display = "block";
+
+    var selectedImageSrc = "ESP/items/" + deck + "/dorso.png";
+    document.getElementById(`selectedImage_${deck}`).src = selectedImageSrc;
+
+    clearDropdownAndImageItems(deck);
+    populateImageDropdown(deck);
+}
+
+function openHechizosPage() {
+    document.getElementById('mainMenu').style.display = 'none';
+    document.getElementById('encuentrosSubmenu').style.display = 'none';
+    document.getElementById('mobiliarioSubmenu').style.display = 'none';
+    document.getElementById('itemsSubmenu').style.display = 'none';
+    document.getElementById("languageButton").style.display = "none";
+    document.getElementById('infoButton').style.display = 'none';
+
+    document.getElementById('hechizosFrame').src = 'spells_ES.html';
+    document.getElementById('hechizosPage').style.display = 'block';
+}
+
+
+// Info Button
+var infoWindow = null;
+
+function openInfoWindow() {
+    // Check if the info window is already open
+    if (infoWindow && !infoWindow.closed) {
+        infoWindow.focus(); // If open, focus on the existing window
+    } else {
+        // If not open, open a new window with the specified text
+        infoWindow = window.open('info.html', '_blank', 'width=400,height=200');
+    }
+}
+
+// Expansion
+var includeExpansion = true;
+
+function toggleExpansion() {
+    var isChecked = document.getElementById("expansionCheckbox").checked;
+    localStorage.setItem("includeExpansion", isChecked);
+    includeExpansion = document.getElementById("expansionCheckbox").checked;
+}
+
+// Switch to English version
+function openEnglishPage() {
+    window.location.href = 'index_EN.html';
+}
+
+
+
